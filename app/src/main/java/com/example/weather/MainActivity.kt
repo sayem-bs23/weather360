e35abdbe3c98d5b75251da2d08a84663415fcaac
sayem-bs23 <sayem.pasha@bs-23.net>
2019-10-31 20:02:38 +0600

    api call through retrofit singleton, showing retrieved data

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -4,18 +4,34 @@ import android.content.Context
 import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_ten_days.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.ArrayList
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity() {
+    private var weatherData: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +45,62 @@ class MainActivity : AppCompatActivity() {
         adapter.addFragment(TenDaysFragment(), "Ten days")
         viewPager.adapter = adapter
         tabs.setupWithViewPager(viewPager)
+        weatherData = findViewById(R.id.msg)
+
+        getCurrentData()
 
     }
+
+    companion object{
+        val AppId= "bb4dd20e432ae754a84c5b91ee475854"
+        val lat = "23"
+        val lon = "90"
+    }
+
+    internal fun getCurrentData() {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(logging)
+        val retrofit = RetrofitSingleton.instance
+        val call = retrofit.getCurrentWeatherData(lat, lon, AppId)
+
+        call.enqueue(object : Callback<WeatherResponse> {
+            override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
+
+                Log.d("resp", "dbg")
+                if (response.code() == 200) {
+                    val weatherResponse = response.body()!!
+
+                    val stringBuilder = "Country: " +
+                            weatherResponse.sys!!.country +
+                            "\n" +
+                            "Temperature: " +
+                            weatherResponse.main!!.temp +
+                            "\n" +
+                            "Temperature(Min): " +
+                            weatherResponse.main!!.temp_min +
+                            "\n" +
+                            "Temperature(Max): " +
+                            weatherResponse.main!!.temp_max +
+                            "\n" +
+                            "Humidity: " +
+                            weatherResponse.main!!.humidity +
+                            "\n" +
+                            "Pressure: " +
+                            weatherResponse.main!!.pressure
+
+                    weatherData!!.text = stringBuilder
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
+                weatherData!!.text = t.message
+            }
+        })
+    }
+
 
 
 
---
+++ app/src/main/java/com/example/weather/RetrofitSingleton.kt
@@ -0,0 +1,47 @@
+package com.example.weather
+
+import android.util.Base64
+import android.util.Log
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitSingleton{
+
+
+    private const val BASE_URL = "http://api.openweathermap.org/"
+
+
+    val instance: WeatherService by lazy{
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(logging)
+
+        var retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient.build())
+            .build()
+
+        retrofit.create(WeatherService::class.java)
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
